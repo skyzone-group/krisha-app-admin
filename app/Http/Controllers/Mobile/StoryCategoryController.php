a47f6f2a035bca5d1b3883b130d0f2b1b6649924
nodirsattorov <nodirbek.sattorov.96@gmail.com>
2024-04-01 12:14:40 +0500

    story response changed

--- app/Http/Controllers/Mobile/StoryCategoryController.php
+++ app/Http/Controllers/Mobile/StoryCategoryController.php
@@ -11,8 +11,20 @@ class StoryCategoryController extends ResponseController
 {
     public function list(Request $request)
     {
+        $lang = app()->getLocale();
         $items = StoryCategory::query();
-        $items = $items->with('items')->get();
-        return self::successResponse($items);
+//        $items = $items->with('items')->get();
+
+        //order by position if position is the same sort by id
+        $items = $items->orderBy('position')->orderBy('id')->get();
+        $data = [];
+        foreach ($items as $item) {
+            $data[] = [
+                'id' => $item->id,
+                'title' => $item->{'title_' . $lang},
+                'image' => $item->photo,
+            ];
+        }
+        return self::successResponse($data);
     }
 }
--- app/Http/Controllers/Mobile/StoryItemController.php
+++ app/Http/Controllers/Mobile/StoryItemController.php
@@ -11,6 +11,7 @@ class StoryItemController extends ResponseController
 {
     public function list(Request $request)
     {
+        $lang = app()->getLocale();
         $v = $this->validate($request->all(),[
             'story_category_id' => 'required'
         ]);
@@ -21,7 +22,34 @@ class StoryItemController extends ResponseController
         $items = StoryItem::query();
         $items = $items->where('story_category_id', $story_category_id);
         $items = $items->get();
-        return self::successResponse($items);
+
+        $data = [];
+        foreach ($items as $item) {
+            $action_type = 'none';
+            $action_data = null;
+            if (!is_null($item->estate_id) && $item->estate_id > 0) {
+                $action_type = 'estate_detail';
+                $action_data = $item->estate_id;
+            }
+            if (!is_null($item->link)) {
+                $action_type = 'link';
+                $action_data = $item->link;
+            }
+
+            $data[] = [
+                'id' => $item->id,
+                'type' => in_array(strtoupper($item->file_type), ['MP4', 'MOV', 'AVI']) ? 'video' : 'image',
+                'source' => $item->file,
+                'action' => $action_type == 'none' ? null : [
+                    'title' => $item->{'title_' . $lang},
+                    'subtitle' => $item->{'subtitle_' . $lang},
+                    'type' => $action_type,
+                    'data' => $action_data
+                ]
+            ];
+        }
+
+        return self::successResponse($data);
     }
 
     public function view(Request $request)
